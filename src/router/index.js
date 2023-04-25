fc846e1ce7319a88b35520061f9f17134d5ddb09
hanyusok <hanyusok@gmail.com>
2023-04-26 00:53:38 +0900

    sigin 고치자

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router"
 import Default from '../views/Default.vue'
+import Signin from '../views/Signin.vue'
 
 const routes = [
   {
     path: "/",
+    name: "Default",
     component: Default  
+  },
+  {
+    path: "/signin",
+    name: "Signin",
+    component: Signin 
   }
 ];
 
