e972d811a727f7102e0dd37491266f54c613f0c7
hanyusok <hanyusok@gmail.com>
2023-05-15 22:11:22 +0900

    datatables

--- src/views/clinic/ClinicList.vue
+++ src/views/clinic/ClinicList.vue
@@ -61,19 +61,25 @@
           </div>
           <div class="px-0 pb-0 card-body">
             <div class="table-responsive">
-              <table id="clinic-table" class="table table-flush">
-                <!--                 <thead class="thead-light">
+              <DataTable :data="clinics" :columns="columns"  :options="options" class="display">
+                <thead class="thead-light">
                   <tr>
-                    <th>clinicName</th>
-                    <th>category</th>
-                    <th>doctorName</th>
-                    <th>phone</th>
-                    <th>title</th>
-                    <th>Status</th>
-                    <th>Action</th>
+                    <th>클리닉</th>
+                    <th>소개</th>
+                    <th>원장</th>
+                    <th>전화번호</th>
                   </tr>
-                </thead> -->
-              </table>
+                </thead>
+                <tfoot>
+                  <tr>
+                    <th>클리닉</th>
+                    <th>소개</th>
+                    <th>원장</th>
+                    <th>전화번호</th>
+                  </tr>
+                </tfoot>
+              </DataTable>
+
 
             </div>
           </div>
@@ -84,105 +90,62 @@
 </template>
 
 <script setup>
-import { DataTable } from "simple-datatables";
+import DataTable from 'datatables.net-vue3'
+import DataTablesCore from 'datatables.net-bs5'
+import languageKO from 'datatables.net-plugins/i18n/ko.json'
+import 'datatables.net-responsive'
+import 'datatables.net-select'
 // import setTooltip from "@/assets/js/tooltip.js";
 import { onMounted, ref, reactive } from "vue";
 import { db } from '@/firebase'
 import { query, onSnapshot, collection, orderBy } from "firebase/firestore";
 
+DataTable.use(DataTablesCore)
+const options = {
+  responsive: true,
+  select: true,
+  language: languageKO
+}
+const columns = [
+  { data: 'clinicName'},
+  { data: 'description'},
+  { data: 'doctorName'},
+  { data: 'phone'},
+]
+
+
 //firestore setting
 const colRef = collection(db, "clinics")
 const q = query(colRef, orderBy('createdAt', 'asc'))
 const clinics = reactive([])
-console.log(clinics)
+// console.log(clinics)
 
 const unsub = onSnapshot(q, (snap) => {
   snap.docChanges().forEach((change) => {
     let changedata = change.doc.data()
     changedata.id = change.doc.id
-    //datatable temporary constant
-    let objt = { data: [] }
-
     if (change.type === "added") {
       clinics.unshift(changedata)
-      console.log(`id: ${changedata.id} added.`)
-      console.log(`clinics length is ${clinics.length}`)
-
-      //add data to datatables
-      
-        if (!objt.headings) {
-          objt.headings = Object.keys(changedata)
-        }
-        console.log(objt.headings)
-      
-      let i = clinics.length - 1
-      objt.data[i] = Object.values(changedata)
-      console.log(objt)
-      
-
-
+      console.log(`id: ${changedata.id} added.`)  
     }
     if (change.type === "modified") {
       let index = clinics.findIndex(listRef => listRef.id === changedata.id)
       Object.assign(clinics[index], changedata)
       console.log(`id: ${changedata.id} modified.`)
-
     }
     if (change.type === "removed") {
       let index = clinics.findIndex(listRef => listRef.id === changedata.id)
       clinics.splice(index, 1)
       console.log(`id: ${changedata.id} modified.`)
     }
-    //simple datatable 
 
-    /*     for (let i = 0; i < clinics.length; i++) {
-          objt.data[i] = [];
-          for (let p in clinics[i]) {
-            if (clinics[i].hasOwnProperty(p)) {
-              objt.data[i].push(clinics[i][p])
-            }
-          }
-        } */
-    const datatable = new DataTable("#clinic-table", {
-      searchable: true,
-      fixedHeight: true,
-      data: objt,
-    })
   })
 },
   (err) => { console.log(err) }
 )
-console.log('firestore connected')
 
 
-/* onMounted(() => {
 
-  //simple datatable 
-  let obj = {
-    headings: Object.keys(clinics[0] || {}),
-    data: []
-  }
-  console.log(`obj(before) headings is ${obj.headings} `)
-
-  for (let i = 0; i < clinics.length; i++) {
-    obj.data[i] = [];
-    for (let p in clinics[i]) {
-      if (clinics[i].hasOwnProperty(p)) {
-        obj.data[i].push(clinics[i][p])
-      }
-    }
-  }
-  console.log(`obj(after) is ${obj.data.data}`)
-  console.log(`clinics length is ${clinics.length}`)
-
-  const datatable = new DataTable("#clinic-table", {
-    searchable: true,
-    fixedHeight: true,
-    data: obj,
-  })
-  console.log(`datatable mounted`)
-  console.log(`clinics length is ${clinics.length}`)
-}) */
 
 
 /* onMounted(() => {
@@ -202,3 +165,7 @@ console.log('firestore connected')
     setTooltip()
   }) */
 </script>
+<style>
+@import 'bootstrap';
+@import 'datatables.net-bs5';
+</style>
